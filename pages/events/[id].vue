64c7c2b8694f968126292397be54bf7d1b9460c5
Maria Rios Navarro <propscode254@gmail.com>
2024-02-25 18:34:14 +0100

    add comment

--- pages/events/[id].vue
+++ pages/events/[id].vue
@@ -1,7 +1,10 @@
 <script setup>
+//how to acces to the params in script
 const route = useRoute();
-
 console.log(route.params);
+
+//const nuxt = useNuxtApp()
+//console.log(nuxt)
 </script>
 <template>
   <div>event id: {{ $route.params.id }}</div>
